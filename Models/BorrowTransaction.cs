56de9af206db873a6e97899cb8aa6c879745c634
Daniel <daniel.macalindong@gmail.com>
2024-08-05 18:00:25 +0300

    new borrowtransaction class

---
+++ Models/BorrowTransaction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSysyem.Models
+{
+    class BorrowTransaction
+    {
+        public int Transactionid { get; set; }
+        public int UserID { get; set; }
+        public int ISBM { get; set; }
+        public DateTime BorrowDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+
+        public BorrowTransaction(int transactionid, int userid, int isbm, DateTime borrowdate, DateTime? returnDate)
+        {
+            Transactionid = transactionid;
+            UserID = userid;
+            ISBM = isbm;
+            BorrowDate = borrowdate;
+            ReturnDate = returnDate;
+        }
+        
+
+        public void BorrowBook()
+        {
+            // Yo
+        }
+
+        public void ReturnBook()
+        {
+            // Yo
+        }
+    }
+}
